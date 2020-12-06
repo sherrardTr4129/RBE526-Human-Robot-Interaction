6b712b561fe72d96c89c17e2cb8e76fce90d2690
yangjack <yyang17@wpi.edu>
2020-12-06 12:24:02 -0500

    Added algorithm implementation

---
+++ hri_control/src/algo.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+
+import rospy
+import math as m
+from hri_control.msg import joint_angle
+from geometry_msgs.msg import Pose
+import geometry_msgs
+from gazebo_msgs.srv import GetModelState, GetLinkState
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import time
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
+class algo:
+
+    def __init__(self):
+        rospy.init_node('estPos', anonymous=True)
+        # self.angle = rospy.Publisher('/desired_angle_right',joint_angle ,queue_size=1)
+        self.can_name = ['coke_can', 'coke_can_clone_1']
+        self.can_pos = []
+        self.state = 'find'
+        self.last_state = 'find'
+        self.pub_cam = rospy.Publisher('/camArmPoseGoal', Pose, queue_size=1)
+        self.pm_x = 0
+        self.pm_y = 0
+        self.d_r = 0
+        self.beta = 0
+        self.L = 0
+        self.d_i = 0
+        self.cam_pose = Pose()
+        self.start_x = 0
+        self.last_Ls = 0
+        self.Ls = 0
+        rospy.Subscriber('/currentCamArmPose', Pose, self.update_pose, queue_size=1)
+        robot_prefix = 'trina2_1'
+        rospy.Subscriber(robot_prefix + '/right_arm_cam/color/image_raw', Image, self.right_image, queue_size=1)
+        self.bridge = CvBridge()
+
+    def right_image(self,data):
+        self.right_cv_image = self.bridge.imgmsg_to_cv2(data,'bgr8')
+
+    def get_beta_L_di_pm(self):
+        try:
+            model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+            self.can_pos = []
+            for i in self.can_name:
+                result = model_coordinates(i, '')
+                self.can_pos.append([result.pose.position.x, result.pose.position.y, result.pose.position.z])
+
+        except rospy.ServiceException as e:
+            rospy.loginfo("Get Model State service call failed:  {0}".format(e))
+        x_distance = self.can_pos[0][0]-self.can_pos[1][0]
+        y_distance = self.can_pos[0][1]-self.can_pos[1][1]
+        self.pm_x = (self.can_pos[0][0]+self.can_pos[1][0])/2
+        self.pm_y = (self.can_pos[0][1] + self.can_pos[1][1]) / 2
+        self.L = m.sqrt(x_distance*x_distance+y_distance*y_distance)
+
+        self.beta = m.atan2(abs(self.can_pos[0][0]-self.can_pos[1][0]),abs(self.can_pos[0][1]-self.can_pos[1][1]))
+        self.d_i = abs((self.pm_y-self.right_ee_post[1]))
+        self.start_x = self.pm_x
+        rospy.loginfo('beta: '+str(self.beta))
+        rospy.loginfo('L: ' + str(self.L))
+        rospy.loginfo('Di: '+str(self.d_i))
+
+
+    def get_right_ee_pos(self):
+        rospy.loginfo('get right ee pos')
+        ee_pos = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
+        right_cam = ee_pos('trina2_1/right_arm_bracelet_link', '')
+        right_cam_x = right_cam.link_state.pose.position.x
+        right_cam_y = right_cam.link_state.pose.position.y
+        right_cam_z = right_cam.link_state.pose.position.z
+
+        self.right_ee_post = [right_cam_x, right_cam_y, right_cam_z]
+
+
+    def update_pose(self,p):
+        self.cam_pose = p
+
+
+
+    def get_dr_alpha(self):
+        self.get_right_ee_pos()
+        x_distance = self.pm_x - self.right_ee_post[0]
+        y_distance = self.pm_y - self.right_ee_post[1]
+        self.d_r = m.sqrt(x_distance*x_distance+y_distance*y_distance)
+        delta_x = self.right_ee_post[0]-self.start_x
+        self.turn = m.atan2(abs(delta_x),abs(self.d_i))
+        self.alpha = m.atan2(abs(delta_x),abs(self.d_i))+self.beta
+        rospy.loginfo('dr: '+str(self.d_r))
+        rospy.loginfo('alpha: ' + str(self.alpha))
+
+
+    def cal_Ls(self):
+        Lv = self.L * m.sin(self.alpha)*m.cos(self.alpha-self.beta)
+        D = 2*self.d_i-self.L*m.cos(self.alpha)*m.cos(self.alpha-self.beta)
+        self.Ls = Lv/D
+
+    def move_robot(self):
+        self.get_dr_alpha()
+        self.last_Ls = self.Ls
+        self.cal_Ls()
+        newPose = self.cam_pose
+        rospy.loginfo("y:" + str(newPose.position.y))
+        if abs(self.Ls-self.last_Ls)>0.001:
+            cv2.imwrite('algo_test/ls'+str(self.Ls)+'.jpg',self.right_cv_image)
+        yaw= 1.57-self.turn
+        rospy.loginfo("yaw:" + str(yaw))
+        #arr = quaternion_from_euler(1.57, 0, yaw)
+        arr = quaternion_from_euler(2.1, 0, yaw)
+        ox = arr[0]
+        oy = arr[1]
+        oz = arr[2]
+        ow = arr[3]
+        newPose.orientation.w = ow
+        newPose.orientation.x = ox
+        newPose.orientation.y = oy
+        newPose.orientation.z = oz
+
+
+        pose_goal = geometry_msgs.msg.Pose()
+        pose_goal.orientation.w = ow
+        pose_goal.orientation.x = ox
+        pose_goal.orientation.y = oy
+        pose_goal.orientation.z = oz
+        pose_goal.position.x = newPose.position.x
+        pose_goal.position.y = newPose.position.y+0.05
+        pose_goal.position.z = newPose.position.z
+        self.pub_cam.publish(pose_goal)
+
+
+
+    def run(self):
+
+        rospy.spin()
+
+    def home_robot(self):
+        newPose = Pose()
+        # x =0.6
+        # y = -0.1
+        # z=0.8
+        # r=1.57
+        # p = 0
+        # yaw = 1.57
+
+        x = -0.05
+        y = 0
+        z = 1.37
+        r = 2.1
+        p = 0
+        yaw = 1.57
+
+
+        arr = quaternion_from_euler(r, p, yaw)
+        ox = arr[0]
+        oy = arr[1]
+        oz = arr[2]
+        ow = arr[3]
+
+        newPose.orientation.w = ow
+        newPose.orientation.x = ox
+        newPose.orientation.y = oy
+        newPose.orientation.z = oz
+
+
+        newPose.position.x = x
+        newPose.position.y = y
+        newPose.position.z = z
+        self.pub_cam.publish(newPose)
+
+
+if __name__ == '__main__':
+    algo = algo()
+    comm_rate = 10
+    rate = rospy.Rate(comm_rate)
+    t_end = time.time()+20
+    while time.time()<t_end:
+        algo.home_robot()
+    rospy.loginfo('start')
+    algo.get_right_ee_pos()
+    algo.get_beta_L_di_pm()
+    algo.get_dr_alpha()
+
+    while not rospy.is_shutdown():
+        if int(time.time())%5 == 0:
+            algo.move_robot()
--- hri_control/src/estimateState.py
+++ hri_control/src/estimateState.py
@@ -3,66 +3,132 @@
 import rospy
 import math as m
 from hri_control.msg import joint_angle
-
+from geometry_msgs.msg import Pose
+import geometry_msgs
 from gazebo_msgs.srv import GetModelState, GetLinkState
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
 
 class estPos:
 
-	def __init__(self):
-		rospy.init_node('estPos',anonymous=True)
-		#self.angle = rospy.Publisher('/desired_angle_right',joint_angle ,queue_size=1)
-		self.can_name = ['coke_can','coke_can_clone','coke_can_clone_0','coke_can_clone_1','coke_can_clone_2',
-						 'coke_can_clone_3']
-		self.can_pos = []
-		self.left_ee_post = []
+    def __init__(self):
+        rospy.init_node('estPos', anonymous=True)
+        # self.angle = rospy.Publisher('/desired_angle_right',joint_angle ,queue_size=1)
+        self.can_name = ['coke_can', 'coke_can_clone', 'coke_can_clone_0', 'coke_can_clone_1', 'coke_can_clone_2',
+                         'coke_can_clone_3']
+        self.can_pos = []
+        self.left_ee_post = []
+        self.right_ee_post = []
+        self.state = 'find'
+        self.last_state = 'find'
+        self.pub_cam = rospy.Publisher('/camArmPoseGoal', Pose, queue_size=1)
 
 
-	def get_left_ee_pos(self):
-		ee_pos = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-		result = ee_pos('trina2_1/left_arm_left_inner_finger','')
-		ee_x = result.link_state.pose.position.x
-		ee_y = result.link_state.pose.position.y
-		ee_z = result.link_state.pose.position.z
-		# global_x = 0.4 - ee_y
-		# global_y = -0.4 + ee_x
-		# global_z = ee_z
-		#rospy.loginfo("ex: "+str(ee_x)+"  ey: "+str(ee_y)+"  ez: "+str(ee_z))
-		#rospy.loginfo("x: "+str(global_x)+"  y: "+str(global_y)+"  z: "+str(global_z))
-		self.left_ee_post = [ee_x,ee_y,ee_z]
+    def get_left_ee_pos(self):
+        ee_pos = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
+        left_finger = ee_pos('trina2_1/left_arm_left_inner_finger', '')
+        left_finger_x = left_finger.link_state.pose.position.x
+        left_finger_y = left_finger.link_state.pose.position.y
+        left_finger_z = left_finger.link_state.pose.position.z
 
-	def cal_distance(self):
-		distance = 100
-		for i in range(0,6):
-			d_x = self.left_ee_post[0] - self.can_pos[i][0]
-			d_y = self.left_ee_post[1] - self.can_pos[i][1]
-			d_z = self.left_ee_post[2] - self.can_pos[i][2]
-			distance = min(distance,m.sqrt(d_x*d_x+d_y*d_y+d_z*d_z))
-		rospy.loginfo("Shortest distance = " + str(distance))
+        right_finger = ee_pos('trina2_1/left_arm_right_inner_finger', '')
+        right_finger_x = right_finger.link_state.pose.position.x
+        right_finger_y = right_finger.link_state.pose.position.y
+        right_finger_z = right_finger.link_state.pose.position.z
+
+        self.left_ee_post = [left_finger_x, left_finger_y, left_finger_z]
+        self.right_ee_post = [right_finger_x, right_finger_y, right_finger_z]
+
+    def cal_distance(self):
+        distance = 100
+        ee_mid_x = (self.left_ee_post[0] + self.right_ee_post[0]) / 2.
+        ee_mid_y = (self.left_ee_post[1] + self.right_ee_post[1]) / 2.
+        ee_mid_z = (self.left_ee_post[2] + self.right_ee_post[2]) / 2.
+        for i in range(0, 6):
+            d_x = ee_mid_x - self.can_pos[i][0]
+            d_y = ee_mid_y - self.can_pos[i][1]
+            d_z = ee_mid_z - self.can_pos[i][2]
+            distance = min(distance, m.sqrt(d_x * d_x + d_y * d_y + d_z * d_z))
+
+        d_f = abs(self.left_ee_post[1] - self.right_ee_post[1])
+        if distance < 0.1 and d_f <= 0.08 and ee_mid_z > 0.9:
+            self.state = 'stack'
+        elif distance < 0.1 and d_f > 0.08:
+            self.state = 'grab'
+        else:
+            self.state = 'find'
+
+    def print_all_pos(self):
+        try:
+            model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+            self.can_pos = []
+            for i in self.can_name:
+                result = model_coordinates(i, '')
+                self.can_pos.append([result.pose.position.x, result.pose.position.y, result.pose.position.z])
+
+        except rospy.ServiceException as e:
+            rospy.loginfo("Get Model State service call failed:  {0}".format(e))
+
+    def run(self):
+
+        rospy.spin()
+
+    def pub_cam_pose(self):
+        newPose = Pose()
+        self.state = 'grab'
+        if self.state == 'find':
+            x = -0.05
+            y = 0
+            z = 1.37
+            r = 2.1
+            p = 0
+            yaw = 1.57
+        elif self.state == 'grab':
+            x = 0.5
+            y = 0
+            z = 1.36
+            r = 3
+            p = 0.1
+            yaw = 1.57
+        else:
+            x = 0.1
+            y = -0.35
+            z = 1.25
+            r = 2.1
+            p = 0
+            yaw = 2.1
 
 
-	
-	def print_all_pos(self):
-		try:
-			model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-			self.can_pos = []
-			for i in self.can_name:
-				result = model_coordinates(i,'')
-				self.can_pos.append([result.pose.position.x,result.pose.position.y,result.pose.position.z])
+        arr = quaternion_from_euler(r, p, yaw)
+        ox = arr[0]
+        oy = arr[1]
+        oz = arr[2]
+        ow = arr[3]
 
-		except rospy.ServiceException as e:
-			rospy.loginfo("Get Model State service call failed:  {0}".format(e))
+        newPose.orientation.w = ow
+        newPose.orientation.x = ox
+        newPose.orientation.y = oy
+        newPose.orientation.z = oz
 
-	def run(self):
-		print "Node running"
-		rospy.spin()
+
+        newPose.position.x = x
+        newPose.position.y = y
+        newPose.position.z = z
+        self.pub_cam.publish(newPose)
+        rospy.loginfo("pub: " + self.state)
 
 
 if __name__ == '__main__':
-	estPos = estPos()
-	comm_rate = 10
-	rate = rospy.Rate(comm_rate)
-	while not rospy.is_shutdown():
-		estPos.print_all_pos()
-		estPos.get_left_ee_pos()
-		estPos.cal_distance()
-		rate.sleep()
+    estPos = estPos()
+    comm_rate = 10
+    rate = rospy.Rate(comm_rate)
+
+    while not rospy.is_shutdown():
+        estPos.pub_cam_pose()
+        estPos.print_all_pos()
+        estPos.get_left_ee_pos()
+        estPos.cal_distance()
+        if estPos.state != estPos.last_state:
+            estPos.pub_cam_pose()
+            estPos.last_state = estPos.state
+        rate.sleep()
